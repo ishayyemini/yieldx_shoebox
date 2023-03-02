f647738b95d079198ccbc849268d5929ca718d22
Ishay Yemini <ishay.yemini@gmail.com>
2023-03-02 12:45:16 +0200

    Fetch get_report_data.js in batches

--- lambdas/get_report_data.js
+++ lambdas/get_report_data.js
@@ -1,8 +1,11 @@
 const sql = require('mssql')
 
-const get_report_data = async ({ username, UID }) => {
+const get_report_data = async ({ username, UID, offset }) => {
   if (username === 'all') username = ''
-  console.log(`Fetching data of test number ${UID}`)
+  if (!offset) offset = 0
+  console.log(`Fetching data of test number ${UID}, offsetting by ${offset}`)
+
+  const _fetchAmount = 5000
 
   const config = {
     user: 'sa',
@@ -13,25 +16,57 @@ const get_report_data = async ({ username, UID }) => {
   }
   await sql.connect(config).catch((e) => console.log(e))
 
-  const records = await new sql.Request()
+  const [records, countRes] = await new sql.Request()
     .query(
-      `SELECT * 
-       FROM SensorsData
-       WHERE dateCreated = (
-         SELECT dateCreated
-         FROM Locations
-         WHERE UID = ${UID} 
-         ${username ? `and Customer = '${username}'` : ''}
-       )`
+      `
+SELECT * 
+FROM SensorsData
+WHERE dateCreated = (
+  SELECT dateCreated
+  FROM Locations
+  WHERE UID = ${UID} 
+  ${username ? `and Customer = '${username}'` : ''}
+)
+ORDER BY UID
+OFFSET ${offset} ROWS
+FETCH NEXT ${_fetchAmount} ROWS ONLY
+
+${
+  !offset
+    ? `
+SELECT Count(*) as row_count
+FROM SensorsData
+WHERE dateCreated = (
+  SELECT dateCreated
+  FROM Locations
+  WHERE UID = ${UID} 
+  ${username ? `and Customer = '${username}'` : ''}
+)
+`
+    : ''
+}
+      `
     )
-    .then((res) => res.recordset)
+    .then((res) => {
+      console.log(res.rowsAffected)
+      if (res.err) console.log(res.err)
+      return res.recordsets || [[], []]
+    })
     .catch((e) => console.log(e))
 
-  sql.close()
+  // sql.close()
 
-  console.log(`Done, fetched ${records.length} records`)
+  console.log(`Done, fetched ${records?.length} records`)
+  const nextOffset =
+    records?.length === _fetchAmount ? offset + records?.length : null
 
-  return records
+  if (!nextOffset) sql.close()
+
+  const requestsNeeded = !offset
+    ? Math.ceil((countRes?.[0]?.row_count || 1) / _fetchAmount) - 1
+    : null
+
+  return { requestsNeeded, records, nextOffset }
 }
 
 module.exports.default = get_report_data
